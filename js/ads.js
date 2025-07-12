83ddde62588c53c915844ab2aea8db87727b02b4
MolibDen25 <ostapenkodo25@gmail.com>
2025-07-12 12:46:08 +0300

    V0.0.3 S25m07d12b

--- js/ads.js
+++ js/ads.js
@@ -4,9 +4,9 @@ document.addEventListener("DOMContentLoaded", () => {
     const adContainer = document.getElementById("ad-container");
   
     const videos = [
-      "https://youtu.be/bALXg6Pv53E",
-      "https://youtu.be/3xqm--MYGV4",
-      "https://youtu.be/1SLguJRqm1g"
+      "https://www.youtube.com/embed/bALXg6Pv53E",
+      "https://www.youtube.com/embed/3xqm--MYGV4",
+      "https://www.youtube.com/embed/1SLguJRqm1g"
     ];
   
     const randomVideo = videos[Math.floor(Math.random() * videos.length)];
